1b2c7f22c2fccc9be4da732c1c16014a4b98e830
Owen Roberts <oroberts@mac34996.bmcc.cuny.edu>
2017-12-19 18:46:04 -0500

    working on example for link

--- examples/lab/game.js
+++ examples/lab/game.js
@@ -1,6 +1,6 @@
 /* global variable */
 let character, platform;
-let clouds, walls, enemies, health, stuff;
+let clouds, walls, enemies, health, stuff, trees;
 const NUM_BUSHES = 8,
     NUM_CLOUDS = 2,
     NUM_WALLS = 3,
@@ -63,6 +63,7 @@ function setup() {
     clouds = new Group();
     enemies = new Group();
     health = new Group();
+    trees = new Group();
 
     /* character setup */
     character = createSprite(0, 20, 16, 16);
@@ -77,8 +78,43 @@ function setup() {
 
     /* platform setup */
     platform = createSprite(0, height - 10, width * 2, 20);
+    platform.rate = floor(random(6,10))
+    platform.space = floor(random(4,8));
+    platform.color = color(random(0,50), random(150,255), random(100,200));
+    //platform.debug = true;
+    platform.draw = function() {
+        if (frameCount % this.rate == 0) {
+            this.space = floor(random(4,8));
+        }
+        stroke(this.color);
+        push();
+        translate(-this.width/2,-this.height/2);
+        for (let x = 0; x <= this.width; x += this.space) {
+            line(x, 0, 0, x);
+            if (x != this.x)
+                line(x, 0 + this.width, 0 + this.width, x);
+        }
+        pop();
+    }
     stuff.add(platform);
 
+    for (let i = 0; i < NUM_CLOUDS; i++) {
+        const cloud = createSprite(
+            random(width, width * 2),
+            random(0, height / 2),
+            random(5,100),
+            random(2,4)
+        );
+        const cloud_img = loadImage(cloud_files[floor(random(0, cloud_files.length))]);
+        cloud.addImage(cloud_img);
+        cloud.velocity.x = -random(cloudSpeedMin, cloudSpeedMax);
+        clouds.add(cloud);
+    }
+
+    buildLevel();
+}
+
+function buildLevel() {
     for (let i = 0; i < NUM_WALLS; i++) {
         const wall = createSprite(
             random(i * width / NUM_WALLS, (i + 1) * width / NUM_WALLS),
@@ -86,41 +122,74 @@ function setup() {
             40,
             height / 4
         );
+        wall.rate = floor(random(6,10))
+        wall.space = floor(random(4,8));
+        wall.color = color(random(50,150), random(50,100), random(200,255));
+      //  wall.debug = true;
+        wall.draw = function() {
+            if (frameCount % this.rate == 0) {
+                this.space = floor(random(4,8));
+            }
+            stroke(this.color);
+            push();
+            translate(-this.width/2,-this.height/2);
+            for (let x = 0; x <= this.width; x += this.space) {
+                line(x, 0, 0, x);
+                if (x != this.x)
+                    line(x, 0 + this.width, 0 + this.width, x);
+            }
+            translate(0,this.height/2);
+            for (let x = 0; x <= this.width; x += this.space) {
+                line(x, 0, 0, x);
+                if (x != this.x)
+                    line(x, 0 + this.width, 0 + this.width, x);
+            }
+            pop();
+        }
         walls.add(wall);
     }
     for (let i = 0; i < NUM_BUSHES; i++) {
-        createSprite(
-            random(0, width),
-            random(height - 20, height),
-            random(10, 60),
-            random(50, 100)
+        const tree = createSprite(
+            random(0, width), 
+            random(height-20, height), 
+            random(10, 20), 
+            random(5, 100)
         );
+        tree.rate = floor(random(6,10))
+        tree.space = floor(random(4,8));
+        tree.color = color(random(255), random(255), random(255));
+     //   tree.debug = true;
+        tree.draw = function() {
+            if (frameCount % this.rate == 0) {
+                this.space = floor(random(4,8));
+            }
+            stroke(this.color);
+            push();
+            translate(-this.width/2,-this.height/2);
+            for (let y = 0; y <= this.height; y += this.height/this.width) {
+                for (let x = 0; x <= this.width; x += this.space) {
+                    line(x, y, 0, y + x);
+                    if (x != this.x)
+                        line(x, 0 + this.width, 0 + this.width, x);
+                }
+            }
+            pop();
+        }
+        trees.add(tree);
     }
-    for (let i = 0; i < NUM_CLOUDS; i++) {
-        const cloud = createSprite(
-            random(width, width * 2),
-            random(0, height / 2),
-            random(50, 100),
-            random(20, 40)
-        );
-        const cloud_img = loadImage(cloud_files[floor(random(0, cloud_files.length))]);
-        cloud.addImage(cloud_img);
-        cloud.velocity.x = -random(cloudSpeedMin, cloudSpeedMax);
-        clouds.add(cloud);
-    }
+
     for (let i = 0; i < NUM_ENEMIES; i++) {
         spawnEnemy();
     }
     for (let i = 0; i < NUM_HEALTH; i++) {
         const life = createSprite(
             random(0, width),
-            random(height / 2, height),
-            30,
+            random(height / 2, height * 3/4),
+            20,
             20
         );
         health.add(life);
     }
-
 }
 
 function spawnEnemy() {
@@ -131,6 +200,11 @@ function spawnEnemy() {
         sz,
         sz
     );
+    enemy.draw = function() {
+        fill(0);
+        rect(0,0,this.width,this.height);
+    }
+    enemy.debug = true; 
     enemy.velocity.x = -random(enemySpeedMin, enemySpeedMax);
     enemies.add(enemy);
 }
@@ -151,8 +225,6 @@ function intro() {
     camera.off();
     background(0);
     fill(255);
-    textSize(24);
-    textAlign(CENTER);
     text("my first game", width / 2, height / 2);
     text("by owen", width / 2, height / 2 + 24);
     text("press enter to start", width / 2, height / 2 + 48);
@@ -165,8 +237,6 @@ function intructions() {
     camera.off();
     background(100);
     fill(255);
-    textSize(24);
-    textAlign(CENTER);
     text("arrow right to move", width / 2, height / 2);
     text("x to jump", width / 2, height / 2 + 24);
     text("press enter to start", width / 2, height / 2 + 48);
@@ -179,8 +249,6 @@ function end() {
     camera.off();
     background(100);
     fill(255);
-    textSize(24);
-    textAlign(CENTER);
     text("you died", width / 2, height / 2);
     text("press enter to try again", width / 2, height / 2 + 48);
     if (keyWentDown("ENTER")) {
@@ -199,6 +267,11 @@ function game() {
             cloud.position.x = random(width, width * 2);
             cloud.position.y = random(0, height / 2);
         }
+    }
+
+    for (let i = 0; i < trees.length; i++) {
+        const tree = trees[i];
+        wrap(tree, random(width, width*3));
     }
 
     /* keyboard events */
@@ -236,10 +309,9 @@ function game() {
         const enemy = enemies[i];
         if (character.overlap(enemy)) {
             character.lives--;
-            // enemy.remove();
-            enemy.position.x += random(width * 2, width * 6);
+            enemy.position.x += random(width * 2, width * 4);
         } else {
-            wrap(enemy, random(width * 2, width * 6));
+            wrap(enemy, random(width * 2, width * 5));
         }
     }
     for (let i = 0; i < health.length; i++) {
@@ -254,11 +326,11 @@ function game() {
     }
 
     /* wrapping sprites */
-    wrap(platform, width);
     for (let i = 0; i < walls.length; i++) {
         const wall = walls[i];
         wrap(wall, random(width * 2, width * 4));
     }
+     wrap(platform, width);
 
 
     /* camera follows character */
@@ -268,22 +340,35 @@ function game() {
     drawSprites(walls);
     drawSprites(enemies);
     drawSprites(health);
+    drawSprites(trees);
 
-    /* ui */
+    
     camera.off();
     drawSprites(clouds);
+
+    /* ui */
     fill(0);
     textAlign(LEFT);
     textSize(12);
     text("Lives: " + character.lives, 10, 20);
 
     /* detect game ending */
-    if (character.lives <= 0) {
+    if (character.lives <= 0 || character.position.y > height + 100) {
         gameState = 3;
         character.velocity.y = 0;
-        /* follow progress */
-
+        character.position.x = 0;
+        character.position.y = 20;
+        platform.position.x = 0;
+        enemies.clear();
+        health.clear();
+        walls.clear();
+        trees.clear();
+        enemySpeedMin = SPEED / 5;
+        enemySpeedMax = SPEED;
+        buildLevel();
     }
+
+    /* follow progress */
     if (character.position.x > width * 5 * progress) {
 		progress++;
 		enemySpeedMin++;
