b7820b573f4740ab5e0611704884c52e0953989a
Owen Roberts <oroberts@f706-i-34138.bmcc.cuny.edu>
2018-11-20 17:01:16 -0500

    week 12 lab

---
+++ labs/18f/week12/scenes.js
@@ -0,0 +1,93 @@
+var scene = "intro";
+
+function draw() {
+    if (scene == "intro") {
+        intro();   
+    } else if (scene == "game") {
+        game();
+    } else if (scene == "restart") {
+        restart();
+    }
+}
+
+function intro() {
+    background("black");
+    
+    fill("white");
+    textAlign(CENTER, CENTER);
+    textFont("Comic Sans MS");
+    textSize(50);
+    
+    text("My First Game", width/2, height/2);
+    text("Press Enter to Start", width/2, height/2 + 100);
+}
+
+function restart() {
+    background("black");
+    
+    fill("white");
+    textAlign(CENTER, CENTER);
+    textFont("Comic Sans MS");
+    textSize(50);
+    
+    text("You Died :(", width/2, height/2);
+    text("Enter to Start Over", width/2, height/2 + 100);
+}
+
+function died() {
+    scene = "restart";
+    
+    // reset progress
+    progress = 0;
+    platformYChange = 20;
+    platformSpeed = speed;
+    
+    // play death sound
+    deathSound.play();
+    
+    // play bg music again
+    bgMusic.play();
+    
+    // stop game music
+    bgGame.stop();
+    
+    // reset the player
+    player.position.x = playerXStart;
+    player.position.y = playerYStart;
+    player.velocity.y = 0;
+    player.livesLeft = 3;
+    
+    // destroy other sprites
+    while (platforms.length > 0) {
+        platforms[0].remove();
+    }
+    
+    while (clouds.length > 0) {
+        clouds[0].remove();
+    }
+    
+    while (trees.length > 0) {
+        trees[0].remove();
+    }
+    
+    while (arrows.length > 0) {
+        arrows[0].remove();
+    }
+}
+
+function keyPressed() {
+    // enter key
+    if (keyCode == 13) {
+        if (scene == "intro" || scene == "restart") {
+            build();
+           
+            // pause start scene background music
+            bgMusic.pause();
+            // start game music
+            bgGame.play();
+            // play sound to start game
+            startSound.play();
+            scene = "game";
+        }
+    }
+}
